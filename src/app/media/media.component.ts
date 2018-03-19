20ca246b4ae3c6eb1feef817324c90bd129bba5e
OnlyOne <dzbik.konrad@gmail.com>
2018-03-19 19:57:32 +0100

    tmp

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -5,10 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './media.component.html',
   styleUrls: ['./media.component.css']
 })
+
 export class MediaComponent implements OnInit {
 
   constructor() { }
 
+  // getFoldersList(){
+  //   const testFolder = '../../';
+  //   const fs = require('fs');
+
+  //   fs.readdir(testFolder, (err, files) => {
+  //     files.forEach(file => {
+  //       console.log(file);
+  //     });
+  //   })
+  // };
+
+  getFolderList2() {
+    // var fs = require('fs');
+    // fs.readdir('.', function (err, files) {
+    //   console.log(files);
+    // });
+    var http = require('http');
+    var uc = require('upper-case');
+    http.createServer(function (req, res) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(uc("Hello World!"));
+      res.end();
+    }).listen(8080);
+    //alert("ATATATATATAAT");
+  };
   ngOnInit() {
   }
 
